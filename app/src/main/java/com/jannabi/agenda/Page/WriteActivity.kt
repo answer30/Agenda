76cfb56c24552c82f59745af085d477ea4227195
answere3030 <answere3030@gmail.com>
2022-01-01 23:59:57 +0900

    Initial commit

---
+++ app/src/main/java/com/jannabi/agenda/Page/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.jannabi.agenda.Page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jannabi.agenda.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
